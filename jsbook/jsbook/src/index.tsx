8a4dcacd3fb274f85148e7134445adbf67ee2831
Maxwell Mandina MAC <maxwell.mandina@gmail.com>
2022-11-21 16:59:32 -0500

    Implemented with old Esbuild API

---
+++ jsbook/jsbook/src/index.tsx
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    console.log(result);
+    setCode(result.code);
+  };
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+let root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
